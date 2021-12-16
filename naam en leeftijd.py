3e83a073f7280d0555ac83d3f7f1741d0a0705eb
Dimert010 <89783123+Dimert010@users.noreply.github.com>
2021-12-16 12:19:34 +0100

    Add files via upload

---
+++ naam en leeftijd.py
@@ -0,0 +1,10 @@
+# def function():
+#     name = input('what is your name? ')
+#     age = input('what is your age? ')
+#     print(name, 'is', age, 'years old' )
+# function()
+
+def function(name, age):
+    print(name, age)
+
+function(input('what is your name? '), input('what is your age? '))
