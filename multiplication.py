678c1b65c40b5b0e21169714cfb194ee939f8847
Dimert010 <89783123+Dimert010@users.noreply.github.com>
2021-12-16 12:18:59 +0100

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,25 @@
+def addition(o, e):
+    return o + e
+total = addition(8, 122)
+print(total)
+def subtraction(u, h):
+    return u - h
+total = subtraction(25, 9)
+print(total)
+def multiplication(l, m):
+    return l * m
+total = multiplication(124, 9)
+print(total)
+def division(q, a):
+    return q / a
+total = division(14, 2)
+print(total)
+def increment(number):
+    return number +1 
+total = increment(14)   
+print(total)
+def decrement(number):
+    return number -1
+total = decrement(21)
+print(total)
+
